5a4df7a5bf299a2d0c0bd30ad758f2c13c8c36dc
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-06-08 11:33:18 +0100

    Update dependencies

--- app/src/main/java/com/example/routinechecks/MainActivity.kt
+++ app/src/main/java/com/example/routinechecks/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.routinechecks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Start new routine activity when FAB is clicked
+        newRoutine.setOnClickListener {
+            val intent = Intent(this, NewRoutineActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
