ae57be99e2f68bcffa413c47c0800f4337305468
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-06-08 22:56:38 +0100

    Create Room database

---
+++ app/src/main/java/com/example/routinechecks/RoutineRoomDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.routinechecks
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Routine::class], version = 1)
+@TypeConverters(Converters::class)
+public abstract class RoutineRoomDatabase : RoomDatabase() {
+    abstract fun routineDao(): RoutineDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RoutineRoomDatabase? = null
+
+        fun getDatabase(context: Context): RoutineRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RoutineRoomDatabase::class.java,
+                    "ROUTINE_DATABASE"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
