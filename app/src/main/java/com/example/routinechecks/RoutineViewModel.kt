33a9de3bb5b8d471665b129739cbdbb2c3795f7a
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-06-09 22:12:44 +0100

    feat: Create ViewModel

---
+++ app/src/main/java/com/example/routinechecks/RoutineViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.routinechecks
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class RoutineViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: Repository
+    private val allRoutines: LiveData<List<Routine>>
+
+    init {
+        val routineDao = RoutineRoomDatabase.getDatabase(application).routineDao()
+        repository = Repository(routineDao)
+        allRoutines = repository.allRoutines
+    }
+
+    fun addRoutine(routine: Routine) = viewModelScope.launch(Dispatchers.IO) {
+        repository.addRoutine(routine)
+    }
+}
