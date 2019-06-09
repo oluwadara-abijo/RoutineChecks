61cb418be0f63301d9ec2d979341acdbc19e9db1
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-06-09 22:10:08 +0100

    feat: Parcelize Routine data class

--- app/src/main/java/com/example/routinechecks/Routine.kt
+++ app/src/main/java/com/example/routinechecks/Routine.kt
@@ -1,15 +1,18 @@
 package com.example.routinechecks
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
+@Parcelize
 @Entity(tableName = "routines_table")
 data class Routine(
     @PrimaryKey val title: String,
     val description: String? = null,
     val frequency: String,
-    val missedRoutines: Int,
-    val completedRoutines: Int,
+    val missedRoutines: Int = 0,
+    val completedRoutines: Int = 0,
     val startTime: Date? = null
-)
+) : Parcelable
