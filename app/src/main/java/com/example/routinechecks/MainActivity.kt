a9076337806ffd8b211e641248f792c2254bf6bd
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-06-10 13:34:14 +0100

    feat: Set click listener on routine items

--- app/src/main/java/com/example/routinechecks/MainActivity.kt
+++ app/src/main/java/com/example/routinechecks/MainActivity.kt
@@ -4,17 +4,20 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RoutineListAdapter.ItemClickListener {
 
     //ViewModel member variable
     private lateinit var mViewModel: RoutineViewModel
+
+    //List of routines
+    private lateinit var mRoutines: List<Routine>
 
     //Request code
     companion object {
@@ -25,9 +28,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mRoutines = ArrayList()
+
         //Set up recycler view
         val recyclerView = routinesList
-        val adapter = RoutineListAdapter(this)
+        val adapter = RoutineListAdapter(mRoutines, this)
         val layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = layoutManager
@@ -39,7 +44,7 @@ class MainActivity : AppCompatActivity() {
 
         //Get all routines in database
         mViewModel.allRoutines.observe(this, Observer { routines ->
-            routines?.let { adapter.setWords(routines) }
+            routines?.let { adapter.setRoutines(routines) }
         })
 
         //Start new routine activity when FAB is clicked
@@ -55,6 +60,17 @@ class MainActivity : AppCompatActivity() {
             data?.let {
                 val routine: Routine = it.getParcelableExtra(NewRoutineActivity.EXTRA_ROUTINE)
                 mViewModel.addRoutine(routine)
+            }
+        }
+    }
+
+    override fun onItemClick(routine: Routine, listenerType: RoutineListAdapter.ListenerType) {
+        when (listenerType) {
+            is RoutineListAdapter.ListenerType.RoutineClickListener -> {
+                Toast.makeText(this, "Item clicked", Toast.LENGTH_SHORT).show()
+            }
+            is RoutineListAdapter.ListenerType.EditClickListener -> {
+                Toast.makeText(this, "Edit icon clicked", Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/com/example/routinechecks/RoutineListAdapter.kt
+++ app/src/main/java/com/example/routinechecks/RoutineListAdapter.kt
@@ -1,35 +1,58 @@
 package com.example.routinechecks
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item_routine.view.*
 
-class RoutineListAdapter internal constructor(context: Context) : RecyclerView.Adapter<RoutineListAdapter.WordViewHolder>() {
+class RoutineListAdapter(private var routines: List<Routine>, private val clickListener: ItemClickListener) :
+    RecyclerView.Adapter<RoutineListAdapter.RoutineViewHolder>() {
 
-    private val inflater: LayoutInflater = LayoutInflater.from(context)
-    private var routines = emptyList<Routine>() // Cached copy of routines
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoutineViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.list_item_routine, parent, false)
+        return RoutineViewHolder(view)
 
-    inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val routineTitleView: TextView = itemView.findViewById(R.id.routineTitle)
-        val routineDescriptionView: TextView = itemView.findViewById(R.id.routineDescription)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-        val itemView = inflater.inflate(R.layout.list_item_routine, parent, false)
-        return WordViewHolder(itemView)
-    }
-
-    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RoutineViewHolder, position: Int) {
         val currentRoutine = routines[position]
-        holder.routineTitleView.text = currentRoutine.title
-        holder.routineDescriptionView.text = currentRoutine.description
+        holder.bind(currentRoutine)
 
     }
 
-    internal fun setWords(routines: List<Routine>) {
+    //Class to handle click interactions
+    sealed class ListenerType {
+        class RoutineClickListener(val routine: Routine) : ListenerType()
+        class EditClickListener (val routine: Routine) : ListenerType()
+    }
+
+    //Class to handle item clicks
+    interface ItemClickListener{
+        fun onItemClick(routine: Routine, listenerType: ListenerType)
+    }
+
+    inner class RoutineViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        override fun onClick(v: View?) {
+            val pos = adapterPosition
+            val routineClicked = routines[pos]
+            var listenerType : ListenerType = ListenerType.RoutineClickListener(routineClicked)
+            when (v?.id) {
+                R.id.editIcon -> listenerType = ListenerType.EditClickListener(routineClicked)
+            }
+            clickListener.onItemClick(routineClicked, listenerType )
+        }
+
+        fun bind(routine: Routine) {
+            itemView.routineTitle.text = routine.title
+            itemView.routineDescription.text = routine.description
+            itemView.editIcon.setOnClickListener(this)
+            itemView.setOnClickListener(this)
+        }
+    }
+
+    internal fun setRoutines(routines: List<Routine>) {
         this.routines = routines
         notifyDataSetChanged()
     }
